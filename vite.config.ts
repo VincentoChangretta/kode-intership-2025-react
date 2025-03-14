94968edf3c95e18a0258948ef1e1e0eda3d85c9d
Aram Papikyan <onlyphilosophylover@gmail.com>
2025-03-15 00:48:19 +0200

    fc

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/kodeOnline/',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
